4e8b79dd59fa133651b16fcf831c7d82d646c7b1
Janusz Nowak <janono@gmail.com>
2021-09-11 00:07:44 +0200

    Adding unittests.

---
+++ test/janono.ado.testcase.associate.unittests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace janono.ado.testcase.associate.unittests;
+[TestClass]
+[janono.ado.testcase.associate.Organization("janono-pub")]
+public class UnitTest1
+{
+    [TestMethod]
+    [janono.ado.testcase.associate.TestCase(5)]
+    public void CheckIfClassMethodHaveAttribiutes()
+    {
+        var method = typeof(UnitTest1).GetMethods()
+        .FirstOrDefault(x => x.Name == nameof(UnitTest1.CheckIfClassMethodHaveAttribiutes));
+        var attribute = method?.GetCustomAttributes(typeof(janono.ado.testcase.associate.TestCaseAttribute), true)
+            .FirstOrDefault() as janono.ado.testcase.associate.TestCaseAttribute;
+
+
+        Assert.IsTrue(attribute?.testCaseId.ToString() == "5");
+        Assert.IsTrue(attribute?.testCaseTitle == null);
+
+
+        var dnAttribute = typeof(UnitTest1).GetCustomAttributes(typeof(janono.ado.testcase.associate.OrganizationAttribute), true)
+        .FirstOrDefault() as janono.ado.testcase.associate.OrganizationAttribute;
+
+        Assert.IsTrue(dnAttribute?.OrganizationName == "janono-pub");
+
+
+    }
+}
